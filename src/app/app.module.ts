e8a919895c721f085c3dc100b0d1b1921a66968e
Burle-vineeth <vineethburle@gmail.com>
2023-03-24 17:37:45 +0530

    anything

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,30 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import {MatIconModule} from '@angular/material/icon'; 
 import { AppComponent } from './app.component';
 import { ErrorComponent } from './userEntry/error/error.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
-import { MatRadioModule } from '@angular/material/radio';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { HttpClientModule } from '@angular/common/http';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { HeaderComponent } from './header/header.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LayoutModule } from '@angular/cdk/layout';
 import { HomeComponent } from './home/home.component';
-import {MatCardModule} from '@angular/material/card';
 import { AddgroceryComponent } from './addItem/addgrocery/addgrocery.component';
-import { CartComponent } from './cart/cart.component'; 
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import { PlaceOrderComponent } from './place-order/place-order.component'; 
-import {MatStepperModule} from '@angular/material/stepper'; 
-import {MatDatepickerModule} from '@angular/material/datepicker'; 
-import { MatNativeDateModule } from '@angular/material/core';
+import { CartComponent } from './cart/cart.component';
+import { PlaceOrderComponent } from './place-order/place-order.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MytasksComponent } from './mytasks/mytasks.component';
 import { AddfruitComponent } from './addItem/addfruit/addfruit.component';
 import { AddObjComponent } from './addItem/add-obj/add-obj.component';
@@ -36,6 +25,7 @@ import { LoginComponent } from './userEntry/login/login.component';
 import { SignupComponent } from './userEntry/signup/signup.component';
 import { EditItemComponent } from './userEntry/edit-item/edit-item.component';
 import { SingleItemComponent } from './single-item/single-item.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -60,23 +50,12 @@ import { SingleItemComponent } from './single-item/single-item.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatCardModule,
     BrowserAnimationsModule,
-    MatStepperModule,
-    MatIconModule,
-    MatInputModule,
-    MatSnackBarModule,
-    MatProgressSpinnerModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatRadioModule,
-    MatFormFieldModule,
     HttpClientModule,
     ReactiveFormsModule,
     FormsModule,
     LayoutModule,
+    MaterialModule,
     NgbModule,
   ],
   providers: [
---
+++ src/app/material.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatStepperModule } from "@angular/material/stepper";
+
+@NgModule({
+    exports: [
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatCardModule,
+        MatStepperModule,
+        MatIconModule,
+        MatInputModule,
+        MatSnackBarModule,
+        MatProgressSpinnerModule,
+        MatButtonModule,
+        MatSelectModule,
+        MatRadioModule,
+        MatFormFieldModule,
+    ],
+    imports: [
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatCardModule,
+        MatStepperModule,
+        MatIconModule,
+        MatInputModule,
+        MatSnackBarModule,
+        MatProgressSpinnerModule,
+        MatButtonModule,
+        MatSelectModule,
+        MatRadioModule,
+        MatFormFieldModule,
+    ]
+})
+
+export class MaterialModule {
+
+}
